279848b1f91c2cfc2c94f28edd55be012e6f5d02
Avalon Lin <Avalon@Avalonde-MacBook-Pro.local>
2017-01-16 10:25:03 +0800

    Fixing bug and enhancing features

--- server.R
+++ server.R
@@ -7,11 +7,11 @@
 
 library(shiny)
 library(data.table)
-library(magrittr)
 library(plyr)
 library(dplyr)
 library(rvest)
 library(stringr)
+library(checkenc)
 
 shinyServer(function(input, output) {
   
@@ -22,10 +22,11 @@ shinyServer(function(input, output) {
     if (is.null (inFile1)) {
       return (NULL)
     } else {
+      coding1 <- lapply(inFile1$datapath, checkenc) %>% unlist %>% unique
       lapply(inFile1$datapath, function(k) {
-        read.csv(k, fileEncoding = "big5", stringsAsFactors = FALSE)} ) %>%
+        read.csv(k, fileEncoding = coding1, stringsAsFactors = FALSE)} ) %>%
         rbind.fill %>% setDT %>% return
-    }
+    } 
   })
   
   stocking.file <- reactive({
@@ -33,12 +34,13 @@ shinyServer(function(input, output) {
       return (NULL)
     } else {
       inFile2 <- input$stockfile
+      coding2 <- checkenc(inFile2$datapath)
       read.csv(inFile2$datapath, stringsAsFactors = FALSE,
-               fileEncoding = "big5") %>% setDT %>% return
+               fileEncoding = coding2) %>% setDT %>% return
     }
   })
   
-  filtertingdatafile <- reactive({
+  filteringdatafile1 <- reactive({
     if (is.null (datafile())) {
       return (NULL)
     } else if (input$ProcessingType == "未出貨數量") {
@@ -48,13 +50,22 @@ shinyServer(function(input, output) {
       tmpfile1 <- tmpfile1[!(訂單狀態 == "已取消" | 訂單狀態 == "已完成")]
       tmpfile1 <- tmpfile1[送貨狀態 == "備貨中"]
       calculate <- tmpfile1[, .(unshipping = sum(數量)), .(商品名稱, 選項)]
+      tmpfile2 <- datafile()[, .(商品名稱, 選項, 商店貨號)] %>% unique
+      tmpfile2 <- tmpfile2[! duplicated(tmpfile2[, .(商品名稱, 選項)])]
+      calculate <- merge(tmpfile2, calculate, by = c("商品名稱", "選項"))
       calculate[is.na(選項), 選項 := ""] %>% return
+    } 
+  })
+  
+  filteringdatafile2 <- reactive({
+    if (is.null(datafile())) {
+      return (NULL)
     } else if (input$ProcessingType == "銷售數量") {
       tmpfile2 <- datafile() %>% setDT()
       tmpfile2 <- tmpfile2[訂單日期 >= input$SelectDate[1] &
-                           訂單日期 <= input$SelectDate[2]]
-      tmpfile2 <- tmpfile2[訂單狀態 != "已取消", .(sales = sum(數量)),
-                               .(商品名稱, 選項)]
+                                 訂單日期 <= input$SelectDate[2]]
+      tmpfile2[訂單狀態 != "已取消", .(sales = sum(數量)),
+                               .(商品名稱, 選項)] %>% return
     }
   })
   
@@ -84,17 +95,21 @@ shinyServer(function(input, output) {
     }
   })
   
-  output$minicake <- renderDataTable({
-    if (is.null (filtertingdatafile())) {
+  pluto <- reactive({
+    if (is.null(datafile())) {
       return (NULL)
-    } else {
-      tmpfile2 <- filtertingdatafile()
+    } else if (input$ProcessingType == "未出貨數量") {
+      tmpfile2 <- filteringdatafile1()
+      tmpfile2[grepl(input$itemname, 商品名稱) & grepl(input$spec, 選項) &
+                 grepl(input$itemID, 商店貨號)] %>% return
+    } else if (input$ProcessingType == "銷售數量") {
+      tmpfile2 <- filteringdatafile2()
       tmpfile2[grepl(input$itemname, 商品名稱) & grepl(input$spec, 選項)] %>% 
         return
     }
   })
   
-  output$hugecake <- renderDataTable({
+  martian <- reactive({
     if (is.null (stocking.file())) {
       return (NULL)
     } else {
@@ -102,12 +117,28 @@ shinyServer(function(input, output) {
       pro.threshold <- as.numeric(input$threshold)
       Name.On.Website <- AllProductName()
       Name.On.Website <- str_replace_all(Name.On.Website,
-                                         "(\\[.*?\\])|\\【.*?\\】", "") %>% trimws
-      lowID <- pro.stock.file[選項 == "" & 庫存.數量 <= pro.threshold, 商品名稱]
-      lowID <- str_replace_all(lowID, "(\\[.*?\\])|\\【.*?\\】", "") %>% trimws
-      lowID <- lowID[lowID %in% Name.On.Website]
-      data.table(ID = seq_along(lowID), ProductName = lowID) %>% return
+                                      "(\\[.*?\\])|\\【.*?\\】", "") %>% trimws
+      tempcheck <- pro.stock.file[, .(checkmulti = .N > 1), 商品名稱]
+      processed.multi <- pro.stock.file[商品名稱 %in% tempcheck[checkmulti ==
+                                        TRUE, 商品名稱], .SD[-1, ], 商品名稱]
+      processed.notmulti <- pro.stock.file[商品名稱 %in% tempcheck[checkmulti ==
+                                        FALSE, 商品名稱]]
+      processed.stockfile <- rbind.fill(list(processed.multi, processed.notmulti),
+                                        fill = TRUE) %>% setDT
+      processed.stockfile <- processed.stockfile[庫存.數量 < pro.threshold]
+      processed.stockfile[, 商品名稱 := str_replace_all(商品名稱,
+                                "(\\[.*?\\])|\\【.*?\\】", "" ) %>% trimws]
+      processed.stockfile <- processed.stockfile[商品名稱 %in% Name.On.Website]
+      processed.stockfile[, .(商品名稱, 選項)]
     }
+  })
+  
+  output$minicake <- renderDataTable({
+    pluto()
+  })
+  
+  output$hugecake <- renderDataTable({
+    martian()
   })
   
   output$mediumcake <- renderDataTable({
@@ -131,4 +162,23 @@ shinyServer(function(input, output) {
     }
   })
   
+  output$DownloadWarningFile <- downloadHandler(
+    filename = function() {
+      paste('LowStocking', 'csv', sep = ".")
+    },
+    content = function(file) {
+      write.csv(martian(), file, row.names = FALSE, quote = FALSE,
+                fileEncoding = "big5")
+    }
+  )
+  
+  output$PickMe <- downloadHandler(
+    filename = function() {
+      paste('UnshippingStock', 'csv', sep = ".")
+    },
+    content = function(file) {
+      write.csv(pluto(), file, row.names = FALSE, quote = FALSE,
+                fileEncoding = "big5")
+    }
+  )
 })
